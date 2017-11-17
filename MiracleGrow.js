55a405b3b253991daa4f966a1e58e18f5901e746
brosenbusch <8brosenbusch@stpatrick.org>
2017-11-17 14:38:49 -0600

    Update MiracleGrow.js

--- MiracleGrow.js
+++ MiracleGrow.js
@@ -2,3 +2,16 @@ const Tree = require('./Tree.js');
 const Queue = require('./Queue.js');
 const Random = require('./Random.js');
 
+const miracleGrow = function(size){
+  let forestSize = size;
+  let plantingSeeds = new Queue();
+  for (let x=0; x<forestSize;x++){
+    plantingSeeds.enqueue(Random().discreteRangeIn(1,1000));
+  }
+  let forest = new Tree(0);
+  for (let x=0; x<plantingSeeds.length;x++){
+    forest.add(plantingSeeds.dequeue());
+  }
+  return forest;
+}
+module.exports = MiracleGrow;
